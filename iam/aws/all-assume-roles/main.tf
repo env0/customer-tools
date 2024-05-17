fb4a3ff885f99eb4aeab631da30cbbaba4d80d3c
Andrew Way <andrew.way@env0.com>
2024-05-17 08:25:47 -0700

    add both deployer and cost roles

---
+++ iam/aws/all-assume-roles/main.tf
@@ -0,0 +1,55 @@
+
+
+### VARIABLES
+
+
+### RESOURCES 
+
+resource "aws_iam_role" "env0_deployer_role" {
+  name = var.assume_role_name
+
+  max_session_duration = 18000 # env0 requirement, 5 hours for SaaS
+
+  # Change to your policy
+  managed_policy_arns = var.managed_policy_arns
+
+  # 913128560466 is env0's AWS Account ID
+  # see: https://docs.env0.com/docs/connect-your-cloud-account#using-aws-assume-role
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::913128560467:root"
+        },
+        "Action" : "sts:AssumeRole",
+        "Condition" : {
+          "StringEquals" : {
+            "sts:ExternalId" : "${local.org_id}"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    note = "Created through env0 Bootstrap"
+  }
+}
+
+resource "env0_aws_credentials" "credentials" {
+  name        = aws_iam_role.env0_deployer_role.arn #easier to track in the UI which role exactly is being used
+  arn         = aws_iam_role.env0_deployer_role.arn
+}
+
+### 
+
+output "deployer_arn" {
+  value = aws_iam_role.env0_deployer_role.arn
+  description = "deployer role"
+}
+
+# output "cost_role_arn" {
+#   value =
+# }
